6fc57bec17fbccb578392e318e7ad63ec0fa05c8
Andy Berrios <berrios.andyj@gmail.com>
2024-11-29 18:35:39 -0600

    data joined

--- scripts/Explore.R
+++ scripts/Explore.R
@@ -3,4 +3,12 @@ library(tidyverse)
 gold <- read.csv('data/Gold_Historical.csv', skip = 9, header = TRUE)
 silver <- read.csv('data/Silver_Historical.csv', skip = 9, header = TRUE)
 
+full <- left_join(gold, silver, by = 'date', suffix = c('_gd','_sv'), keep = FALSE)
+
+par(mfrow=c(2,2))  
+
+
+
+
+
 
